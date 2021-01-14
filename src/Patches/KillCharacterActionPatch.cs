080491a759272e8c5ddb0d3350eb8b73b5689552
Matthew D. Hall <azrinon@gmail.com>
2021-01-14 12:28:13 -0800

    Converted annotated patches to use Patch

--- src/Patches/KillCharacterActionPatch.cs
+++ src/Patches/KillCharacterActionPatch.cs
@@ -1,77 +1,64 @@
-﻿using System;
-using System.Linq;
+﻿using HarmonyLib;
 
-using HarmonyLib;
+using System;
+using System.Linq;
 
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
-using TaleWorlds.Core;
 
 namespace HousesCalradia.Patches
 {
-    [HarmonyPatch(typeof(KillCharacterAction))]
-    internal sealed class KillCharacterActionPatch
+    internal sealed class KillCharacterActionPatch : Patch
     {
-        internal static class ForOptimizer
-        {
-            internal static void DoApplyInternalPrefix() => ApplyInternalPrefix(null!, null!, KillCharacterAction.KillCharacterActionDetail.None, true);
-        }
+        private static readonly Reflect.Method TargetMethod = new(typeof(KillCharacterAction), "ApplyInternal");
+        private static readonly Reflect.Method<KillCharacterActionPatch> PatchMethod = new(nameof(ApplyInternalPrefix));
 
-        private static class Helpers
-        {
-            internal static bool IsClanExtinctionPreventionDisallowed(Clan? clan, Hero? victim)
-                => clan is null
-                || clan.Leader is null
-                || clan.Leader != victim
-                || clan.Kingdom is null
-                || clan.Kingdom.IsEliminated
-                || clan.IsRebelClan
-                || clan.IsBanditFaction
-                || clan == CampaignData.NeutralFaction
-                || victim == Hero.MainHero
-                || clan.Lords.Where(h => h.IsAlive && !h.IsChild && h.IsActive && h.IsNoble && h != victim).Any();
-        }
+        internal KillCharacterActionPatch() : base(Type.Prefix, TargetMethod, PatchMethod, HarmonyLib.Priority.VeryHigh) { }
 
-        [HarmonyPrefix]
-        [HarmonyPriority(Priority.HigherThanNormal)]
-        [HarmonyPatch("ApplyInternal")]
-        static void ApplyInternalPrefix(
-            Hero? victim,
-            Hero? killer,
-            KillCharacterAction.KillCharacterActionDetail actionDetail,
-            bool showNotification)
+        private static bool IsClanExtinctionPreventionDisallowed(Clan? clan, Hero victim)
+            => clan is null
+            || clan.Leader is null
+            || clan.Leader != victim
+            || clan.Kingdom is null
+            || clan.Kingdom.IsEliminated
+            || clan.IsRebelClan
+            || clan.IsBanditFaction
+            || clan == CampaignData.NeutralFaction
+            || victim == Hero.MainHero
+            || clan.Lords.Any(h => h.IsAlive && !h.IsChild && h.IsActive && h.IsNoble && h != victim);
+
+        private static void ApplyInternalPrefix(Hero? victim,
+                                                Hero? killer,
+                                                KillCharacterAction.KillCharacterActionDetail actionDetail,
+                                                bool showNotification)
         {
             _ = showNotification;
 
-            if (victim is null)
-                return;
-
-            var clan = victim.Clan;
-
             // Only interested in the death of regular clan leaders where there's no other adult noble to succeed them:
-            if (Helpers.IsClanExtinctionPreventionDisallowed(clan, victim))
+            if (victim is null || IsClanExtinctionPreventionDisallowed(victim.Clan, victim))
                 return;
 
-            if (Config.AllowPlayerExecutionToEliminateClan &&
-                killer == Hero.MainHero &&
-                actionDetail == KillCharacterAction.KillCharacterActionDetail.Executed)
+            // If configured, allow player executions to eliminate clans:
+            if (Config.AllowPlayerExecutionToEliminateClan
+                && killer == Hero.MainHero
+                && actionDetail == KillCharacterAction.KillCharacterActionDetail.Executed)
             {
                 return;
             }
 
-            Util.Log.Print($"[{CampaignTime.Now}] CLAN EXTINCTION PREVENTION: Leader of clan {clan.Name}, " +
-                $"{victim.Name} of age {victim.Age:F0}, died without a valid heir (reason: " +
-                $"{Enum.GetName(typeof(KillCharacterAction.KillCharacterActionDetail), actionDetail)})!");
+            Util.Log.Print($"[{CampaignTime.Now}] CLAN EXTINCTION PREVENTION: Leader of clan {victim.Clan.Name},"
+                         + $" {victim.Name} of age {victim.Age:F0}, died without a valid heir (reason:"
+                         + $" {Enum.GetName(typeof(KillCharacterAction.KillCharacterActionDetail), actionDetail)})!");
 
             // Spawn a male noble "distant relative" into the clan
             var ageMin = Math.Max(22, Campaign.Current.Models.AgeModel.HeroComesOfAge + 1);
-            var successor = HeroUtil.SpawnNoble(clan, ageMin, ageMax: ageMin + 10, isFemale: false);
+            var successor = HeroUtil.SpawnNoble(victim.Clan, ageMin, ageMax: ageMin + 10, isFemale: false);
 
             if (successor is null)
                 Util.Log.Print(" -> ERROR: Could not find a noble character template to spawn lord!");
             else
-                Util.Log.Print($" -> Summoned distant relative {successor.Name} of age {successor.Age:F0} " +
-                    $"to assume leadership of clan {successor.Clan.Name}.");
+                Util.Log.Print($" -> Summoned distant relative {successor.Name} of age {successor.Age:F0}"
+                             + $" to assume leadership of clan {successor.Clan.Name}.");
         }
     }
 }
--- src/Patches/RomanceCampaignBehaviorPatch.cs
+++ src/Patches/RomanceCampaignBehaviorPatch.cs
@@ -1,7 +1,5 @@
 ﻿using HarmonyLib;
 
-using System.Runtime.CompilerServices;
-
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 
@@ -11,19 +9,14 @@ namespace HousesCalradia.Patches
     /// Disable periodic, untargeted, unfiltered AI marriages added in e1.5.5, as it's a redundant
     /// and inferior system to the one in Houses of Calradia.
     /// </summary>
-    [HarmonyPatch(typeof(RomanceCampaignBehavior))]
-    internal static class RomanceCampaignBehaviorPatch
+    internal sealed class RomanceCampaignBehaviorPatch : Patch
     {
-        internal static class ForOptimizer
-        {
-            internal static void DoCheckNpcMarriagesPrefix() => CheckNpcMarriagesPrefix(Clan.PlayerClan);
-        }
+        private static readonly Reflect.Method<RomanceCampaignBehavior> TargetMethod = new("CheckNpcMarriages");
+        private static readonly Reflect.Method<RomanceCampaignBehaviorPatch> PatchMethod = new(nameof(CheckNpcMarriagesPrefix));
 
-        [HarmonyPrefix]
-        [HarmonyPriority(Priority.VeryHigh)]
-        [HarmonyPatch("CheckNpcMarriages")]
-        [MethodImpl(MethodImplOptions.NoOptimization)]
-        private static bool CheckNpcMarriagesPrefix(Clan? consideringClan)
+        internal RomanceCampaignBehaviorPatch() : base(Type.Prefix, TargetMethod, PatchMethod, HarmonyLib.Priority.HigherThanNormal) { }
+
+        private static bool CheckNpcMarriagesPrefix(Clan consideringClan)
         {
             _ = consideringClan;
             return false;
